5c5597750b1dbb7e7db7d80b447f6d82728e4220
Leejin Cha <117060645+leejincha@users.noreply.github.com>
2022-12-01 14:59:31 +0900

    스프링입문 첫 주 class설계 과제
    2022.11.25 자바 클래스 설계 과제

---
+++ 1st_week_class/Bus/Bus.java
@@ -0,0 +1,84 @@
+package Bus;
+
+public class Bus {
+    String busNum;
+    int passenger;
+    final int maxPassenger = 30;
+    int fee = 1000;
+    int gas;
+    int speed;
+    String status = "운행";
+
+    Bus(String busNum){
+        this.busNum = busNum;
+    }
+
+  public int gasStaus(int gas){
+        this.gas += gas;
+        return gas;
+    }
+
+
+    public String run(){
+        if(this.gas > 0 && this.status.equals("운행")){
+            status = "운행";
+            return status;
+        }else{
+            status = "차고지행";
+            return status;
+        }
+    }
+
+    public String busStatus(int gas){
+        this.gas = gas;
+        if(this.gas == 0 && run() != "운행"){
+            status = "차고지행";
+            return status;
+        } else if (0 < this.gas && this.gas < 10) {
+            status ="주유가 필요합니다.";
+            return status;
+        } else{
+            status = "운행 중";
+            return status;
+        }
+    }
+
+    public void setPassenger(int passenger){
+        this.passenger = passenger;
+        if(this.passenger <= maxPassenger && run() == "운행") {
+            this.passenger += passenger;
+            System.out.println("탑승이 가능합니다.");
+        }else{
+            System.out.println("최대 승객 수 초과");
+        }
+    }
+
+    public int getFee(int passenger) {
+        this.fee = fee * passenger;
+        return fee;
+    }
+
+    public void getSpeed(int speed, int gas){
+        this.gas = gas;
+        if(this.gas < 10){
+            System.out.println("주유 필요");
+        }else {
+            this.speed = speed;
+        }
+    }
+
+    public static void main(String[] args) {
+     Bus mybus = new Bus("a1111");
+     mybus.gasStaus(20);
+     mybus.setPassenger(6);
+     mybus.getSpeed(20, mybus.gas);
+
+        System.out.println("버스넘버 :" + mybus.busNum);
+        System.out.println("탑승 승객 수: "+ mybus.passenger);
+        System.out.println("잔여 승객 수: "+ (30 - mybus.passenger));
+        System.out.println("요금 확인: " + mybus.getFee(mybus.passenger));
+        System.out.println("주유량: " + mybus.gas);
+        System.out.println("상태: " + mybus.busStatus(mybus.gas));
+
+    }
+}
---
+++ 1st_week_class/Taxi/Taxi.java
@@ -0,0 +1,83 @@
+package Taxi;
+
+public class Taxi {
+
+    String TaxiNum;
+    int passenger;
+    int gas;
+    int distance = 2;
+    String dest = "서울역";
+    int defaultDistance = 1;
+    int defaultFee = 3000;
+    int totalFee;
+    int feePerDistance = 1000;
+    String staus = "일반";
+
+    Taxi(String TaxiNum){
+        this.TaxiNum = TaxiNum;
+    }
+
+    public void gasStaus(int gas){
+        this.gas += gas;
+        if(this.gas<10){
+            System.out.println("주유 필요");
+        }
+    }
+
+    public String run(){
+
+        if(this.gas >= 10 && this.passenger == 0){
+            staus = "일반";
+            return staus;
+        } else if (this.gas >= 10 && 0<this.passenger) {
+            staus = "운행 중";
+            return staus;
+        }
+        else{
+            staus = "운행불가";
+            return staus;
+        }
+    }
+
+    public String getPassenger(int passenger) {
+        this.passenger = passenger;
+
+        if(0 < passenger && passenger < 5) {
+            return "운행중";
+        } else {
+            return "최대 승객 수 초과";
+        }
+    }
+
+    public void overPrice() {
+        if (this.distance > this.defaultDistance) {
+            this.totalFee = (feePerDistance * (distance-1)) + defaultFee;
+        }else{
+            this.totalFee = defaultFee;
+        }
+    }
+
+    public int payment(int totalFee){
+        this.totalFee = totalFee;
+        return totalFee;
+    }
+
+    public static void main(String[] args) {
+        Taxi mytaxi = new Taxi("a1111");
+
+        mytaxi.getPassenger(3);
+        mytaxi.gasStaus(20);
+        mytaxi.overPrice();
+
+
+        System.out.println("택시넘버: "+ mytaxi.TaxiNum);
+        System.out.println("승객수: "+mytaxi.passenger);
+        System.out.println("잔여승객수: "+ (4-mytaxi.passenger) );
+        System.out.println("목적지 :"+mytaxi.dest);
+        System.out.println("목적지까지 거리: " +mytaxi.distance);
+        System.out.println("지불할 요금: "+mytaxi.payment(mytaxi.totalFee));
+        System.out.println("상태: "+ mytaxi.run());
+        System.out.println("주유량: "+mytaxi.gas);
+
+    }
+}
